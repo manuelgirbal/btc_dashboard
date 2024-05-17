dc1d82053d6ac222ded3cbf511ea8a821e9e08ad
manuelgirbal <manuelgirbal@proton.me>
2024-05-17 00:12:22 -0300

    .

--- scripts/transactions.R
+++ scripts/transactions.R
@@ -37,8 +37,8 @@ btctxs_plot <- df_txs %>%
   geom_line() +
   ylab("Transactions") +
   xlab("Date") +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
-  theme(plot.background = element_rect(fill = "#A6A6A6"),
-        panel.background = element_rect(fill = "#A6A6A6"),
-        panel.grid.major = element_line(colour = "#7A7A7A"))
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") # +
+  # theme(plot.background = element_rect(fill = "#A6A6A6"),
+  #       panel.background = element_rect(fill = "#A6A6A6"),
+  #       panel.grid.major = element_line(colour = "#7A7A7A"))
 
